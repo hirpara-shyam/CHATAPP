6ccfa368d3c056c06b9b89449a1d5e77e9cd7d07
hirpara-shyam <hirparashyam6@gmail.com>
2025-07-01 09:14:41 +0530

    Sizing change for mobile

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
                 {/* Mobile Layout */}
                 {!selectedUser && <Sidebar />}
                 {selectedUser && !showRightSidebar && (
-                  <ChatContainer onToggleSidebar={() => setShowRightSidebar(true)} />
+                  <ChatContainer className="pt-[calc(env(safe-area-inset-top, 16px))]" onToggleSidebar={() => setShowRightSidebar(true)} />
                 )}
                 {selectedUser && showRightSidebar && (
                   <RightSidebar onBack={() => setShowRightSidebar(false)} />
